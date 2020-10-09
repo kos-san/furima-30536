fa74e5fdaf83241cab42c90c72264043e5753369
kos-san <makocchya917@gmail.com>
2020-10-10 00:29:52 +0900

    imageのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,15 +12,11 @@ class Item < ApplicationRecord
   validates :name,        presence: true
   validates :description, presence: true
   validates :price,       presence: true
-  # validates :user 後から確認
-  validates :category,    presence: true
-  validates :condition,   presence: true
-  validates :charge,      presence: true
-  validates :area,        presence: true
-  validates :day,         presence: true
+  validates :user,        presence: true
+  validates :image,       presence: true
 
   # id:1を選択できないようにバリデーションの設定
-  validates :category_id, :condition_id, :charge_id, :area_id, :day_id, numericality: { other_than: 1 }
+  validates :category_id, :condition_id, :charge_id, :area_id, :day_id, numericality: { other_than: 1 , message: "Select"}
 
   # 半角英数でなければ登録できない, 金額が300~9,999,999までの範囲で登録する
   validates :price, format: { with: /\A\d{3}\z/, message: 'Price Half-width number' }, numericality: {greater_than: 299,less_than: 10000000, message: 'Price Out of setting range'}
