05be9db3aca323ac2056233af75091a95bcafe89
kos-san <makocchya917@gmail.com>
2020-10-09 14:30:10 +0900

    FactoryBotのインストール

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    nickname = "ニックネーム"
+    first_name = "鈴木"
+    last_name = "太郎"
+    first_name_kana = "スズキ"
+    last_name_kana = "タロウ"
+    birthday = "2000-01-02"
+  end
+end
