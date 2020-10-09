ba7c29a7d87064352571f713e64ba7d4e409d1ad
kos-san <makocchya917@gmail.com>
2020-10-09 21:00:09 +0900

    ActiveHashのインストール

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     email                  { Faker::Internet.free_email }
     password               { Faker::Internet.password(min_length: 6) + '1a' }
     password_confirmation  { password }
-    first_name             {'鈴木'}
+    first_name             { '鈴木'}
     last_name              {'太郎'}
     first_name_kana        {'スズキ'}
     last_name_kana         {'タロウ'}
