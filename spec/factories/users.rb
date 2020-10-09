f069909b494a57812ec728acb669ea0c2960cb54
kos-san <makocchya917@gmail.com>
2020-10-09 16:52:07 +0900

    userモデル単体テストコードの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname = "ニックネーム"
-    first_name = "鈴木"
-    last_name = "太郎"
-    first_name_kana = "スズキ"
-    last_name_kana = "タロウ"
-    birthday = "2000-01-02"
+    nickname               {Faker::Name.name}
+    email                  {Faker::Internet.free_email}
+    password               { Faker::Internet.password(min_length: 6) + "1a"}
+    password_confirmation  {password}
+    first_name             {"鈴木"}
+    last_name              {"太郎"}
+    first_name_kana        {"スズキ"}
+    last_name_kana         {"タロウ"}
+    birthday               {"2000-01-02"}
   end
 end
