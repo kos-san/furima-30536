bfa0def09f3bc8fea52189b175ea543fe51fc1d7
kos-san <makocchya917@gmail.com>
2020-10-10 16:47:10 +0900

    rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,16 +16,13 @@ class Item < ApplicationRecord
   validates :user,        presence: true
 
   # id:1を選択できないようにバリデーションの設定
-  validates :category_id, :condition_id, :charge_id, :area_id, :day_id, numericality: { other_than: 1 , message: "Select"}
+  validates :category_id, :condition_id, :charge_id, :area_id, :day_id, numericality: { other_than: 1, message: 'Select' }
 
   # 半角英数でなければ登録できない, 値が300~9,999,999までの範囲で登録する
-  validates :price, numericality: {greater_than: 299,less_than: 10000000, message: 'Out of setting range'}
-  validates :price, numericality: {message: 'Half-width number'}
+  validates :price, numericality: { greater_than: 299, less_than: 10_000_000, message: 'Out of setting range' }
+  validates :price, numericality: { message: 'Half-width number' }
 
   def was_attached?
-    self.image.attached?
+    image.attached?
   end
-  
 end
-
-
