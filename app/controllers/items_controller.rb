03e40284d758adb09bbd259a4eea42de0e89024b
kos-san <makocchya917@gmail.com>
2020-10-12 12:01:18 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user_id
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -38,7 +36,7 @@ class ItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id])
     if current_user.id == item.user_id
-       item.destroy
+      item.destroy
       redirect_to root_path
     else
       redirect_to root_path
