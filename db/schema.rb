36fdd4db6eb073eb4c0c3fa41954a8078ece4978
kos-san <makocchya917@gmail.com>
2020-10-12 18:14:29 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,19 +33,26 @@ ActiveRecord::Schema.define(version: 2020_10_09_150319) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "price", null: false
-    t.bigint "user_id"
-    t.integer "category_id", null: false
-    t.integer "condition_id", null: false
-    t.integer "charge_id", null: false
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
     t.integer "area_id", null: false
-    t.integer "day_id", null: false
+    t.string "city", null: false
+    t.string "house_num", null: false
+    t.string "tel", null: false
+    t.string "building", null: false
+    t.bigint "buy_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
+    t.index ["buy_id"], name: "index_addresses_on_buy_id"
+  end
+
+  create_table "buys", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "item_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_buys_on_item_id"
+    t.index ["user_id"], name: "index_buys_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
